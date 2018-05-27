ab73541fd8a4a34882050614b1dd420b95923d32
harkib <32686629+harkib@users.noreply.github.com>
2018-05-27 12:44:33 -0700

    Create test to test network.h function

---
+++ C++ network function/test.cpp
@@ -0,0 +1,13 @@
+//test.cpp to test network functions
+# include "network.h"
+# include <iostream>
+
+
+int main() {
+	double arrIn[7];
+	double coeff[63];
+	double arrOut[2];
+
+	cout << arrIn[0] << endl;
+	return 0;
+}
